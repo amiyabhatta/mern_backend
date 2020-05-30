5027c07ab20c68b2884a896f41d5d73824cbc918
amiya-perceptive <amiya@perceptive-solutions.com>
2020-05-30 19:15:50 +0530

    complete categpory part

--- routes/category.js
+++ routes/category.js
@@ -3,11 +3,23 @@ const router = express.Router();
 
 
 
-const {getCategoryById} = require("../controllers/category");
+const {getCategoryById,createCategory,getCategory,getAllCategory,updateCategory,removeCategory} = require("../controllers/category");
 const {getUserById} = require("../controllers/user");
 const {isAdmin,isAuthenticated,isSignIn} = require("../controllers/auth");
 
 router.param("userId",getUserById); 
 router.param("categoryId",getCategoryById); 
 
+router.post("/category/create/:userId",isSignIn,isAuthenticated,isAdmin,createCategory)
+
+router.get("/category/:categoryId",getCategory);
+router.get("/categories",getAllCategory);
+
+//update
+router.put("/category/:categoryId/:userId",isSignIn,isAuthenticated,isAdmin,updateCategory);
+
+//Delete
+router.delete("/category/:categoryId/:userId",isSignIn,isAuthenticated,isAdmin,removeCategory);
+
 module.exports = router;
+
